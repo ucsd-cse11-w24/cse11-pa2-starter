957dcb00e9d723cdb5cda92a341e12301c0d1588
Derek Hwang <60747151+derek-hwang27@users.noreply.github.com>
2022-01-07 19:14:01 -0800

    .

--- run.bat
+++ run.bat
@@ -4,7 +4,7 @@ if "%1"=="" goto help
 
 del *.class
 javac -cp lib/tester.jar *.java
-java -classpath lib/tester.jar;"%cd%" tester.Main %1
+java -Djava.security.manager=disallow -classpath lib/tester.jar;"%cd%" tester.Main %1
 goto :eof
 
 :help
